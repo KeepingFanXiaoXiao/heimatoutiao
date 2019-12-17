5df1af4b0733ae4018427e9617aad74de0eaeaac
范小小 <1263648985@qq.com>
2019-12-17 20:43:43 +0800

    整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui' //  引入element-ui
+import 'element-ui/lib/theme-chalk/index.css' //  引入样式
+import './styles/index.less' //  引入初始化配置
 Vue.config.productionTip = false
-
+//  使用element-ui，全局注册element-UI
+Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App)
---
+++ src/views/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="">
+        我是主页
+    </div>
+</template>
+
+<script>
+export default {
+  name: ''
+}
+</script>
+
+<style>
+</style>
